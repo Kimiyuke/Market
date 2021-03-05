90729a1b306254bfac4cce1605a56805c304b8d3
Andrea SSD <Andrea SSD@AndreaSSD>
2021-03-05 15:47:55 +0100

    prova

--- Object/src/DAOS/FruttaDAO.java
+++ Object/src/DAOS/FruttaDAO.java
@@ -35,7 +35,7 @@ public class FruttaDAO {
           
               }
               catch (SQLException x) {
-          System.out.println("Inserimento frutta panel magazzinoo: " +x);
+          System.out.println("Inserimento frutta panel magazzinooo: " +x);
       
                }
 		
